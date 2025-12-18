cd17ca7bd9757c2e0bfc47bfaf0e5835c16987aa
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-12-18 20:20:16 +0800

    feat: 添加自定义主题配置和配置远程访问的文档

--- docs/.vuepress/collections.ts
+++ docs/.vuepress/collections.ts
@@ -57,7 +57,9 @@ const doc = defineCollection({
   title: 'MSLX 文档',
   // 手动配置侧边栏结构
   sidebar: [
-    { text: '从这里开始', prefix: 'install', collapsed: false ,icon:'download', items: ['start','windows','macos', 'linux' ] },
+    { text: '从这里开始', prefix: 'install', collapsed: false, icon: 'download', items: ['start', 'windows', 'macos', 'linux'] },
+    { text: '个性化', prefix: 'style', collapsed: false, icon: 'palette', items: ['webpanel'] },
+    { text: '配置', prefix: 'config', collapsed: false, icon: 'gear', items: ['remote-access'] },
   ],
   sidebarCollapsed: true,
 })
