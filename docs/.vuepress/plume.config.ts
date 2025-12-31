aff688dc0dfebeeeab8364dd88d85f5dd6076820
luluxiaoyu <ljy.xiaoyu@qq.com>
2025-12-31 14:33:57 +0800

    feat: 2026 新的一年啦~

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -43,7 +43,7 @@ export default defineThemeConfig({
   /* 站点页脚 */
   footer: {
     message: 'Powered by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a>',
-    copyright: 'Copyright © <a href="https://github.com/MSLTeam/">MSLDev</a> 2021-2025 & <a href="https://beian.miit.gov.cn/">粤ICP备2023094648号-2</a>',
+    copyright: 'Copyright © <a href="https://github.com/MSLTeam/">MSLDev</a> 2021-2026 & <a href="https://beian.miit.gov.cn/">粤ICP备2023094648号-2</a>',
    },
 
   /**
