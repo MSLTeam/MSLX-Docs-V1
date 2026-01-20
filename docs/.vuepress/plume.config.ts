2fbb4e8b99d676ed9d3522605f12ad7d11d27952
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-01-20 12:20:34 +0800

    fix: 修复顶部栏图标丢失

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -23,10 +23,10 @@ export default defineThemeConfig({
   appearance: true,  // 配置 深色模式
 
   social: [
-    { icon: 'github', link: 'https://github.com/MSLTeam/MSLX' },
-    { icon: 'qq', link: '/article/qq/' },
+    { icon: 'b:github', link: 'https://github.com/MSLTeam/MSLX' },
+    { icon: 'b:qq', link: '/article/qq/' },
   ],
-  navbarSocialInclude: ['github', 'qq'], // 允许显示在导航栏的 social 社交链接
+  navbarSocialInclude: ['b:github', 'b:qq'], // 允许显示在导航栏的 social 社交链接
   // aside: true, // 页内侧边栏， 默认显示在右侧
   // outline: [2, 3], // 页内大纲， 默认显示 h2, h3
 
