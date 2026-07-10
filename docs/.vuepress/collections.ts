d852535360ec842bd3993f2b9164cf48a9c823e9
luluxiaoyu <ljy.xiaoyu@qq.com>
2026-07-10 22:50:13 +0800

    feat: 新增Docker部署服务端教程

--- docs/.vuepress/collections.ts
+++ docs/.vuepress/collections.ts
@@ -58,7 +58,7 @@ const doc = defineCollection({
   // 手动配置侧边栏结构
   sidebar: [
     { text: '从这里开始', prefix: 'install', collapsed: false, icon: 'download', items: ['start', 'windows', 'macos', 'linux','docker','fnos'] },
-    { text: '开服指南', prefix: 'server', collapsed: false, icon: 'circle-play', items: ['java','bedrock','package','choose-server-tips'] },
+    { text: '开服指南', prefix: 'server', collapsed: false, icon: 'circle-play', items: ['java','bedrock','package','docker','choose-server-tips'] },
     { text: '映射/联机', prefix: 'proxy', collapsed: false, icon: 'rocket', items: ['frp', 'p2p','frp-real-ip','server-no-port'] },
     { text: '服务端配置', prefix: 'server-config', collapsed: false, icon: 'screwdriver-wrench', items: ['crons','yggdrasil'] },
     { text: '个性化', prefix: 'style', collapsed: false, icon: 'palette', items: ['webpanel'] },
